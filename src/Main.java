513caa6792a6b67a549e6a9a2515ae9240975a53
Devavrat-Surve <devsurve1625@gmail.com>
2025-01-19 23:33:13 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -23,26 +23,22 @@ class Calsi {
 
 
         switch (operator) {
-
-            // performs addition between numbers
+            
             case '+':
                 result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + result);
                 break;
 
-            // performs subtraction between numbers
             case '-':
                 result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + result);
                 break;
 
-            // performs multiplication between numbers
             case '*':
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
                 break;
 
-            // performs division between numbers
             case '/':
                 result = number1 / number2;
                 System.out.println(number1 + " / " + number2 + " = " + result);
